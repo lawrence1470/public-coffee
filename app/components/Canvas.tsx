c5bc277b00f8b3812edae147e481dfa9f84f0f65
lawrence nicastro <lawrence.nicastro1@gmail.com>
2025-07-11 16:57:26 -0400

    Replace shirt with mug component and add proper scaling

--- app/components/Canvas.tsx
+++ app/components/Canvas.tsx
@@ -1,33 +1,55 @@
-'use client'
+"use client";
 
-import { useRef } from 'react'
-import { Canvas, useFrame } from '@react-three/fiber'
-import { useGLTF, useTexture, AccumulativeShadows, RandomizedLight, Decal, Environment, Center } from '@react-three/drei'
-import { easing } from 'maath'
-import { useSnapshot } from 'valtio'
-import { state } from '../store'
-import * as THREE from 'three'
+import { useRef } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import {
+  useGLTF,
+  useTexture,
+  AccumulativeShadows,
+  RandomizedLight,
+  Environment,
+  Center,
+} from "@react-three/drei";
+import { easing } from "maath";
+import { useSnapshot } from "valtio";
+import { state } from "../store";
+import { Mug } from "./Mug";
+import * as THREE from "three";
 
-export const CanvasComponent = ({ position = [0, 0, 2.5], fov = 25 }) => (
-  <Canvas shadows camera={{ position, fov }} gl={{ preserveDrawingBuffer: true }} className="canvas">
+export const CanvasComponent = ({
+  position = [0, 0, 2.5] as [number, number, number],
+  fov = 25,
+}) => (
+  <Canvas
+    shadows
+    camera={{ position, fov }}
+    gl={{ preserveDrawingBuffer: true }}
+    className="canvas"
+  >
     <ambientLight intensity={0.5 * Math.PI} />
     <Environment files="https://dl.polyhaven.org/file/ph-assets/HDRIs/hdr/1k/potsdamer_platz_1k.hdr" />
     <CameraRig>
       <Backdrop />
       <Center>
-        <Shirt />
+        <Mug />
       </Center>
     </CameraRig>
   </Canvas>
-)
+);
 
 function Backdrop() {
-  const shadows = useRef<THREE.Group>(null!)
-  useFrame((state, delta) => {
+  const shadows = useRef<THREE.Group>(null!);
+  const snap = useSnapshot(state);
+  useFrame((_, delta) => {
     if (shadows.current) {
-      easing.dampC((shadows.current as any).getMesh().material.color, state.color, 0.25, delta)
+      easing.dampC(
+        (shadows.current as any).getMesh().material.color,
+        snap.color,
+        0.25,
+        delta
+      );
     }
-  })
+  });
   return (
     <AccumulativeShadows
       ref={shadows}
@@ -37,34 +59,45 @@ function Backdrop() {
       scale={5}
       resolution={2048}
       rotation={[Math.PI / 2, 0, 0]}
-      position={[0, 0, -0.14]}>
-      <RandomizedLight amount={4} radius={9} intensity={0.55 * Math.PI} ambient={0.25} position={[5, 5, -10]} />
-      <RandomizedLight amount={4} radius={5} intensity={0.25 * Math.PI} ambient={0.55} position={[-5, 5, -9]} />
+      position={[0, 0, -0.14]}
+    >
+      <RandomizedLight
+        amount={4}
+        radius={9}
+        intensity={0.55 * Math.PI}
+        ambient={0.25}
+        position={[5, 5, -10]}
+      />
+      <RandomizedLight
+        amount={4}
+        radius={5}
+        intensity={0.25 * Math.PI}
+        ambient={0.55}
+        position={[-5, 5, -9]}
+      />
     </AccumulativeShadows>
-  )
+  );
 }
 
 function CameraRig({ children }: { children: React.ReactNode }) {
-  const group = useRef<THREE.Group>(null!)
-  const snap = useSnapshot(state)
+  const group = useRef<THREE.Group>(null!);
+  const snap = useSnapshot(state);
   useFrame((state, delta) => {
-    easing.damp3(state.camera.position, [snap.intro ? -state.viewport.width / 4 : 0, 0, 2], 0.25, delta)
-    easing.dampE(group.current.rotation, [state.pointer.y / 10, -state.pointer.x / 5, 0], 0.25, delta)
-  })
-  return <group ref={group}>{children}</group>
+    easing.damp3(
+      state.camera.position,
+      [snap.intro ? -state.viewport.width / 4 : 0, 0, 2],
+      0.25,
+      delta
+    );
+    easing.dampE(
+      group.current.rotation,
+      [state.pointer.y / 10, -state.pointer.x / 5, 0],
+      0.25,
+      delta
+    );
+  });
+  return <group ref={group}>{children}</group>;
 }
 
-function Shirt(props: any) {
-  const snap = useSnapshot(state)
-  const texture = useTexture(`/${snap.decal}.png`)
-  const { nodes, materials } = useGLTF('/shirt_baked_collapsed.glb')
-  useFrame((state, delta) => easing.dampC((materials as any).lambert1.color, snap.color, 0.25, delta))
-  return (
-    <mesh castShadow geometry={(nodes as any).T_Shirt_male.geometry} material={(materials as any).lambert1} material-roughness={1} {...props} dispose={null}>
-      <Decal position={[0, 0.04, 0.15]} rotation={[0, 0, 0]} scale={0.15} map={texture} />
-    </mesh>
-  )
-}
-
-useGLTF.preload('/shirt_baked_collapsed.glb')
-;['/react.png', '/three2.png', '/pmndrs.png'].forEach(useTexture.preload)
+useGLTF.preload("/mug.glb");
+["/react.png", "/three2.png", "/pmndrs.png"].forEach(useTexture.preload);
---
+++ app/components/Mug.tsx
@@ -0,0 +1,80 @@
+"use client";
+
+import { useFrame } from "@react-three/fiber";
+import { useGLTF, useTexture, Decal } from "@react-three/drei";
+import { easing } from "maath";
+import { useSnapshot } from "valtio";
+import { state } from "../store";
+
+export function Mug(props: any) {
+  const snap = useSnapshot(state);
+  const texture = useTexture(`/${snap.decal}.png`);
+  const { nodes, materials } = useGLTF("/mug.glb") as any;
+
+  // Animate color changes
+  useFrame((_, delta) => {
+    if (materials && materials["01___Default"]) {
+      easing.dampC(materials["01___Default"].color, snap.color, 0.25, delta);
+    }
+    if (materials && materials["01___Default-2"]) {
+      easing.dampC(materials["01___Default-2"].color, snap.color, 0.25, delta);
+    }
+  });
+
+  return (
+    <group {...props} scale={0.1} position={[0, -0.5, 0]} dispose={null}>
+      {/* Main mug body */}
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Arc001_1?.geometry}
+        material={materials["01___Default-2"]}
+      />
+
+      {/* Mug handle and other parts */}
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Arc001_1_1?.geometry}
+        material={materials["02___Default-2"]}
+      />
+
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Arc001_1_2?.geometry}
+        material={materials["02___Default"]}
+      />
+
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Arc001_1_3?.geometry}
+        material={materials["01___Default"]}
+      />
+
+      {/* Decal surface */}
+      <mesh geometry={nodes.Arc001_1_4?.geometry}>
+        <meshBasicMaterial transparent opacity={0} />
+        <Decal
+          position={[0, 1.8, 1]}
+          rotation={[0, 0, 0]}
+          scale={[1.5, 1.5, 1.5]}
+        >
+          <meshStandardMaterial
+            map={texture}
+            toneMapped={false}
+            transparent
+            polygonOffset
+            polygonOffsetFactor={-1}
+          />
+        </Decal>
+      </mesh>
+    </group>
+  );
+}
+
+// Preload the mug model
+useGLTF.preload("/mug.glb");
+// Preload textures
+["/react.png", "/three2.png", "/pmndrs.png"].forEach(useTexture.preload);
